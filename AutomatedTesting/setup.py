a1202a4938fb99ef53c8404433ec2928d0f31707
M0WUT <dan.m0wut@gmail.com>
2022-01-24 22:23:21 +0000

    Moved to installable module

---
+++ AutomatedTesting/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='AutomatedTesting',
+    version='0.0.1',
+    url='https://github.com/M0WUT/Automated-Testing.git',
+    author='Dan McGraw',
+    packages=find_packages(),    
+    install_requires=['pytest', 'pytest-cov', 'pytest-flake8', 'Xlsxwriter', 'pyserial', 'pyvisa', 'pyvisa-py', 'numpy']
+)
