a63835ff762695e405f18b1dc8f10b5622da9450
M0WUT <dan.m0wut@gmail.com>
2022-01-24 22:24:06 +0000

    Correctly added new files

--- AutomatedTesting/TopLevel/config.py
+++ AutomatedTesting/TopLevel/config.py
@@ -5,11 +5,12 @@ from AutomatedTesting.SignalGenerator.Siglent_SDG2122X import Siglent_SDG2122X
 from AutomatedTesting.SpectrumAnalyser.Agilent_E4407B import Agilent_E4407B
 
 tenmaSingleChannel = Tenma_72_2535("ASRL/dev/ttyACM0::INSTR")
-smb100a = RandS_SMB100A("TCPIP::192.168.0.23::INSTR")
+smb100a = RandS_SMB100A("TCPIP::10.59.73.10::INSTR")
 u2001a = Agilent_U2001A("USB0::2391::11032::MY53150007::0::INSTR")
 e4407b = Agilent_E4407B(
     "USB0::1003::8293::Hewlett-Packard__E4407B__SG44210622__A.14.01::0::INSTR",
     enableDisplay=True,
 )
 # e4407b = Agilent_E4407B("GPIB0::18::INSTR", enableDisplay=True)
-sdg2122x = Siglent_SDG2122X("USB::0xf4ec::0x1102::SDG2XCAX5R0800::INSTR")
+#sdg2122x = Siglent_SDG2122X("USB::0xf4ec::0x1102::SDG2XCAX5R0800::INSTR")
+sdg2122x = Siglent_SDG2122X("TCPIP::10.59.73.11::INSTR")
